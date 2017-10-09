a23a031ff6677ec9a750801c4986574f9724c7c0
Merlin Patterson <merlinpatt@gmail.com>
2017-10-09 13:17:55 -0400

    Add basic slides

---
+++ slides/js/revealConfig.js
@@ -0,0 +1,23 @@
+var config = {
+  dependencies: [
+    { src: 'plugin/notes/notes.js', async: true },
+    { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
+  ]
+};
+
+var following = window.location.search.includes('follow');
+var master = window.location.href.includes('localhost:') && ! following;
+if (master || following) {
+  config.multiplex = {
+    secret: following ? null : '15075670032199992251',
+    id: 'f70a7bf070cc4b4e',
+    url: 'http://localhost:1948',
+  };
+
+  config.dependencies = [
+    { src: '//cdn.socket.io/socket.io-1.3.5.js', async: true },
+    { src: 'plugin/multiplex/' + (following ? 'client.js' : 'master.js'), async: true },
+  ].concat(config.dependencies);
+}
+
+Reveal.initialize(config);
